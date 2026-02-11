18e1525b50afb10b79ef29202ce12e3e7bcc4b40
sara-lek <sarathisasikala2006@gmail.com>
2026-02-11 22:38:54 +0530

    fixed finaally

--- src/BookPre/ThreatreList.jsx
+++ src/BookPre/ThreatreList.jsx
@@ -1,6 +1,6 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
-import "./theatrestyle.css";
+import "./threatrestyle.css";
 
 function TheatreList() {
   const { id } = useParams(); // movie id from route
