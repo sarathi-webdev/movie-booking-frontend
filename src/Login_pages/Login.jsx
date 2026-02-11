6597289d30bc9d809a8ccc1222028d7367fc7160
sarathi-dev <sarathisasikala2006@gmail.com>
2026-02-11 21:40:00 +0530

    NOthing

--- src/Login_pages/Login.jsx
+++ src/Login_pages/Login.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "./login.css";
+import "./Login.css";
 import user_icon from "../assets/person.png";
 import user_email from "../assets/email.png";
 import user_pass from "../assets/password.png";
