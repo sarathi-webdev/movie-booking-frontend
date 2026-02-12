620ab4549e06cbdfd6bc0575fc54475c7ffd9039
sara-lek <sarathisasikala2006@gmail.com>
2026-02-12 09:56:58 +0530

    Force refresh file tracking

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 import {BrowserRouter,Routes,Route} from "react-router-dom"
 import Login from './Login_pages/Login'
 import Movie from './MoviesSection/Movie'
